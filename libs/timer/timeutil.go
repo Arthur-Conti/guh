1d1b3bbf727a9bf102fec1fa4d7110a370de3da4
Arthur-Conti <arthurconti2003@gmail.com>
2025-08-07 15:02:43 -0300

    feat: add timer lib

---
+++ libs/timer/timeutil.go
@@ -0,0 +1,65 @@
+package timer
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Arthur-Conti/guh/config"
+	"github.com/Arthur-Conti/guh/libs/log/logger"
+)
+
+type TimerGrade string
+
+var (
+	HorribleGrade TimerGrade = "horrible"
+	OkGrade       TimerGrade = "ok"
+	GoodGrade     TimerGrade = "good"
+	AmazingGrade  TimerGrade = "amazing"
+)
+
+var TimerGradeMap = map[int]TimerGrade{
+	3: HorribleGrade,
+	2: OkGrade,
+	1: GoodGrade,
+	0: AmazingGrade,
+}
+
+type Timer struct {
+	NowTime time.Time
+}
+
+func NewTimer() *Timer {
+	return &Timer{}
+}
+
+func (t *Timer) Start() {
+	t.NowTime = time.Now()
+}
+
+func (t *Timer) End() time.Duration {
+	return time.Since(t.NowTime)
+}
+
+func (t *Timer) EndAndPrint(message string) {
+	fmt.Println(message, time.Since(t.NowTime))
+}
+
+func (t *Timer) ClassifyTime(duration time.Duration) int {
+	if duration > time.Minute {
+		return 3
+	} else if duration > time.Second {
+		return 2
+	} else if duration > time.Millisecond {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func (t *Timer) TestFunction(function func()) {
+	t.Start()
+	function()
+	duration := t.End()
+	grade := TimerGradeMap[t.ClassifyTime(duration)]
+	config.Config.Logger.Infof(logger.LogMessage{ApplicationPackage: "timer", Message: "Function was classified as %v grade. It took: %v\n", Vals: []any{grade, duration}})
+}
